bd6cc89d81ab189e6196080382a1087e17d85b4b
Aryanuser123 <92803075+Aryanuser123@users.noreply.github.com>
2021-10-20 23:58:50 +0530

    Add files via upload

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "customer_detail";
+$username = "id17792552_aryan_user";
+$password = "m5b-XF/2>+k%pZYv";
+$dbname = "id17792552_customer_detail";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
